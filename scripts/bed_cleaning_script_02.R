3eb8f27343262c1a5768e5bc174f8f20529c3366
Weezay36 <louiselaurenson@hotmail.co.uk>
2023-07-10 14:28:04 +0100

    clean_update

--- scripts/bed_cleaning_script_02.R
+++ scripts/bed_cleaning_script_02.R
@@ -5,21 +5,21 @@ library(tidyverse)
 #READING
 
 # reading in initial data
-beds <- janitor::clean_names(read_csv("data/beds_by_nhs_board_of_treatment_and_specialty.csv"))
+beds <- janitor::clean_names(read_csv("raw_data/beds_by_nhs_board_of_treatment_and_specialty.csv"))
 
 # added to replace health board codes with location name
-health_board <- janitor::clean_names(read_csv("data/health_board.csv")) %>% 
+health_board <- janitor::clean_names(read_csv("raw_data/health_board.csv")) %>% 
   select(hb, hb_name)
 # added to replace specialist board codes with location name
-specialist_board <- janitor::clean_names(read_csv("data/special_board.csv")) %>% 
+specialist_board <- janitor::clean_names(read_csv("raw_data/special_board.csv")) %>% 
   select(shb, shb_name)
 #added to replace the hospital codes with location name
-hospital_data <- janitor::clean_names(read_csv("data/hospital.csv")) %>% 
+hospital_data <- janitor::clean_names(read_csv("raw_data/hospital.csv")) %>% 
   select(location, location_name)
 # creting a row to add to health board codes as only one specialist area was a hospital
-add_row <- data.frame(hb = "SB0801", hb_name = "The Golden Jubilee National Hospital")
+#add_row <- data.frame(hb = "SB0801", hb_name = "The Golden Jubilee National Hospital")
 #binding the row 
-health_board <- rbind(health_board, add_row)
+#health_board <- rbind(health_board, add_row)
 
 #CLEANING
 
@@ -32,15 +32,19 @@ beds_clean <- beds %>%
 #replacing the codes with location names
 beds_clean <- merge(beds_clean, hospital_data, by = "location", all.x = TRUE)
 
+beds_clean <- merge(beds_clean, health_board, by = "hb", all.x = TRUE)
+
 #only selecting cols for analysis
 beds_clean <- select(beds_clean, -hb, -specialty, -location, -quarter_qf, -hbqf, -location_qf, -specialty_qf, -specialty_name_qf,
                      -all_staffed_beddays_qf, -average_available_staffed_beds_qf, -average_occupied_beds_qf,
-
-                                          -percentage_occupancy_qf, -total_occupied_beddays_qf)
+                     
+                     -percentage_occupancy_qf, -total_occupied_beddays_qf)
 #reordering cols
 
-beds_clean <- select(beds_clean, year, quarter, location_name, everything())
+beds_clean <- select(beds_clean, year, quarter, location_name, hb_name, everything())
 
 #removing uneeded objects from environment 
 
-rm(add_row,beds,health_board,hospital_data,specialist_board)
+rm(beds,health_board,hospital_data,specialist_board)
+
+write_csv(beds_clean, "clean_data/clean_bed_data")
