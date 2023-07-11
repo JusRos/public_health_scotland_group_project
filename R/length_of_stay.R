c30d171633a3bf0d93a9f7b6e5bdf443cf143c35
Emmz900 <68696398+Emmz900@users.noreply.github.com>
2023-07-11 14:00:08 +0100

    updated length of stay plot

--- R/length_of_stay.R
+++ R/length_of_stay.R
@@ -29,15 +29,22 @@ create_length_of_stay_plot <- function(data, health_board,
            quarter = str_remove(quarter, "[0-9]{4}"))
   
   years <- unique(length_data$year)
+  #min_data <- min(length_data$avg_stay)
     
   length_data %>% 
     ggplot(aes(interaction(quarter, year), avg_stay), size = 5) +
     geom_line(group = 1, size = 2) +
     geom_point(size = 3) +
-    annotate("text", x = seq_len(nrow(length_data)), y = min(length_data$avg_stay) - 0.7, label = length_data$quarter) +
+    annotate("text", x = seq_len(nrow(length_data)),
+             y = -(0.05 * max(length_data$avg_stay)),
+             label = length_data$quarter) +
     annotate("text", x = 1 + 4 * (0:(length(years)-1)),
-             y = min(length_data$avg_stay) - 1.3, label = years) +
-    coord_cartesian(ylim = c(min(length_data$avg_stay), max(length_data$avg_stay)), clip = "off") +
+             y = -(0.13 * max(length_data$avg_stay)),
+             label = years) +
+    coord_cartesian(ylim = c(-0.01, max(length_data$avg_stay) + 0.5),
+                    #xlim = ,
+                    expand = FALSE,
+                    clip = "off") +
     #geom_hline(aes(yintercept = mean_stay,
     #               colour = "Pre-covid 2018-2019 Average"), alpha = 0.5) + 
     # annotate("rect", xmin = 1, xmax = 2, ymin = -Inf, ymax = Inf,
@@ -51,7 +58,6 @@ create_length_of_stay_plot <- function(data, health_board,
     # annotate("rect", xmin = 17, xmax = 18, ymin = -Inf, ymax = Inf,
     #          fill = "steelblue",alpha = 0.5) +
     geom_vline(xintercept = 9, size = 1.5, colour = "red") +
-    
     scale_colour_manual(name = "", # Add legend
                         breaks = "Pre-covid 2018-2019 Average",
                         values = c("Pre-covid 2018-2019 Average" = "red")) +
@@ -59,11 +65,11 @@ create_length_of_stay_plot <- function(data, health_board,
     theme(
       text = element_text(size = 15),
       axis.text.x = element_blank(),
-      axis.title.x = element_text(vjust = -8),
+      axis.title.x = element_text(vjust = -10),
       plot.margin = unit(c(1, 1, 3, 1), "lines")
     ) +
     labs(
-      title = "Average length of stay",
+      title = "Average Length of Stay for Inpatients by Demographic",
       x = "Quarter",
       y = "Average Length of Stay (days)"
     )
