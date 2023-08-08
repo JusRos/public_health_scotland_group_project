f1c9d039a1442070c3079e5c589251323451d9bf
Justyna <131964169+JusRos@users.noreply.github.com>
2023-08-08 11:01:13 +0100

    commit

--- PowerPoint/SIMD.R
+++ PowerPoint/SIMD.R
@@ -7,14 +7,22 @@ simd <- read_csv("clean_data/simd_clean.csv")
 simd %>% 
   mutate(Covid = case_when(
     Quarter %in% c("2017Q4","2018Q1","2018Q2","2018Q3", "2018Q4", "2019Q1", "2019Q2", "2019Q3", "2019Q4", "2020Q1") ~ "Pre-Covid 19",
+<<<<<<< Updated upstream
     Quarter %in% c("2020Q2","2020Q3","2020Q4","2021Q1","2021Q2","2021Q3","2021Q4") ~ "During Covid_19",
+=======
+    Quarter %in% c("2020Q2","2020Q3","2020Q4","2021Q1","2021Q2","2021Q3","2021Q4") ~ "During Covid 19",
+>>>>>>> Stashed changes
     Quarter %in% c("2022Q1", "2022Q2","2022Q3","2022Q4") ~ "Post-Covid 19"
   )) %>% 
   mutate(Season = case_when(
     Quarter_single %in% c("Q2","Q3") ~ "Summer",
     Quarter_single %in% c("Q1","Q4") ~ "Winter"
   )) %>% 
+<<<<<<< Updated upstream
   mutate(Covid = factor(Covid, levels = c("Pre-Covid 19", "During Covid_19", "Post-Covid 19"))) %>% 
+=======
+  mutate(Covid = factor(Covid, levels = c("Pre-Covid 19", "During Covid-19", "Post-Covid 19"))) %>% 
+>>>>>>> Stashed changes
   filter(AdmissionType == "Emergency Inpatients") %>% 
   group_by(SIMD, Season, Covid) %>% summarise(mean = mean(Stays), .groups = "drop") %>%
   ggplot(aes(x = SIMD, y = mean, fill = Covid))+
